5c3931eec17c29f01fab8be8c0f40555aead689a
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-04-05 21:18:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,10 @@ import math
 import pandas as pd
 import streamlit as st
 import docx2txt
-
+Skills_Lab_Supplies_Inventory.docx
+https://github.com/LinkHere/CEU-SOM-INVENTORY/blob/16f6a046ee624297e1885cc99b6639b238d1b1aa/Skills_Lab_Supplies_Inventory.docx
 # extract text
-text = docx2txt.process("https://github.com/LinkHere/CEU-SOM-INVENTORY/blob/main/Skills_Lab_Supplies_Inventory.docx")
+text = docx2txt.process("https://github.com/LinkHere/CEU-SOM-INVENTORY/blob/main/Skills_Lab_Supplies_Inventory.docx?raw=true")
 
 st.text(text)
 
