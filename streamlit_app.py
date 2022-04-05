768f23c7e1c3ebec83aef7c1f95cb5400022ada1
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-04-05 21:49:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import pandas as pd
 import streamlit as st
 import docx2txt
 # extract text
-text = docx2txt.process("https://combinatronics.com/LinkHere/CEU-SOM-INVENTORY/main/Skills_Lab_Supplies_Inventory.docx")
-
-st.text(text)
+#text = docx2txt.process("https://combinatronics.com/LinkHere/CEU-SOM-INVENTORY/main/Skills_Lab_Supplies_Inventory.docx")
+with open('https://raw.githubusercontent.com/LinkHere/streamlit-example/master/requirements.txt') as f:
+    contents = f.readlines()
+st.text(contents)
 
